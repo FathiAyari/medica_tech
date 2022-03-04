dfea396cd807b9e292a345c0f61586e14ef9bcb5
FathiAyari <fathi3ayari333@gmail.com>
2022-03-04 17:45:27 +0100

    project done

--- lib/models/spo2Model.dart
+++
@@ -1,5 +0,0 @@
-class SPo2 {
-  final double value;
-  final DateTime date;
-  SPo2({this.value, this.date});
-}
--- lib/utils/constants.dart
+++
@@ -1,9 +0,0 @@
-const String FONT_POPPINS_LIGHT = "Poppins-Light";
-const String FONT_POPPINS_MEDIUM = "Poppins-Medium";
-const String FONT_POPPINS_BOLD = "Poppins-Bold";
-
-enum DISPLAYED_SECTION {
-  SCREENS,
-  USERS,
-  ADMIN
-}
